a0b5b6ac6b16abbd21c12bfa9252159e18e0b3e1
sara-farsinejad <farsinejadsara@gmail.com>
2025-01-18 09:43:27 +0330

    Update temperature.asm

--- temperature.asm
+++ temperature.asm
@@ -1,17 +1,9 @@
-                                                           
-
-           ;;HAKKI EGEMEN GULPINAR 17-155-014 TEMPERATURE CONTROL PROJECT 8086
-
 org 100h             
 CONTROL EQU 8006H    
 A	EQU 0080H        
 B	EQU 0082H        
 C	EQU 0084H                                     
-
 inic1:                                        
-
-
-
 mov dx,C
 in al,dx
 cmp al,11111110b 
@@ -19,11 +11,7 @@ je stop1
 cmp al,11111101b 
 je decision
 jmp inic1
-
 decision:
-   
-
-
 mov dx,B
 in al,dx
 cmp al,00010001b 
@@ -31,70 +19,44 @@ jge enfriar
 cmp al,00001011b 
 jle warm
 jmp stop
-
-
-
 enfriar: 
-
 mov dx,A
 mov al,11111110b 
 out dx,al
-
-
 nop
 nop 
 nop 
-
 jmp emergency
-
 warm:
-
 mov dx,A
 mov al,11111101b
 out dx,al
-
-
 nop
 nop
 nop
 nop 
-
 jmp emergency
-
 stop:
-
 mov dx,A
 mov al,11111111b 
 out dx,al 
-
-
 nop
 nop
 nop 
-
-
-
 mov dx,B
 in al,dx
 cmp al,00010001b 
 jge enfriar 
 cmp al,00001011b 
 jle warm
-jmp stop 
- 
+jmp stop  
 stop1:
-
 mov dx,A
 mov al,11111111b  
 out dx,al 
-
-
 nop
 nop
 nop 
-
-
-
 mov dx,C
 in al,dx
 cmp al,11111110b 
@@ -102,7 +64,6 @@ je stop1
 cmp al,11111101b 
 je inic1
 jmp stop1
- 
 emergency:                                        
 
 
